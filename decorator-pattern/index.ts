e51119f42755bc75f4953358ff7d07b052beac9f
kjhwert <kjhwert11@naver.com>
2022-04-24 17:24:00 +0900

    decorator pattern

---
+++ decorator-pattern/index.ts
@@ -0,0 +1,34 @@
+export interface Beverage {
+  shot: number;
+  syrup: boolean;
+  topping?: Topping;
+  drizzle?: Drizzle;
+  whipping?: Whipping;
+  order(): Omit<Beverage, "order">;
+}
+
+type Topping = "Java Chip" | "Espresso Chip";
+type Drizzle = "Caramel" | "Chocolate";
+type Whipping = "Normal" | "Espresso";
+
+const beverage: Beverage = {
+  shot: 1,
+  syrup: false,
+  order() {
+    const { order, ...rest } = this;
+    return rest;
+  },
+};
+
+function beverageWithDecorate(beverage: Beverage, args: Partial<Beverage>) {
+  return {
+    ...beverage,
+    ...args,
+  };
+}
+
+const topping = beverageWithDecorate(beverage, { topping: "Java Chip" });
+const drizzle = beverageWithDecorate(topping, { drizzle: "Caramel" });
+const result = beverageWithDecorate(drizzle, { whipping: "Normal" });
+
+console.log(result.order());
